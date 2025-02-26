59c5501a7bade787c5ccd033aba3353b78192a06
quaypro000 <samtacquay1@gmail.com>
2025-02-26 19:31:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ client.on('messageCreate', message => {
   }
 });
 
-// Thêm Express.js để giữ Render hoạt động
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.send('Bot is running!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Web server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Web server is running on port ${port}`);
 });
 
 // Đăng nhập bot Discord
